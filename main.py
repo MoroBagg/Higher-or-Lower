f5020910283f0be66f3f47d65dff45185fedf484
MoroBagg <131180722+MoroBagg@users.noreply.github.com>
2023-09-19 21:34:52 +0100

    Updated main.py
    Fixed quite a major issue in which any input in game type 1 would work and give "correct". Not only this but also a bug that would give "correct" even when it was wrong.

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ if typeofgame == "1":
         
         #generating the enemy player's roll
         aiNum = random.randint(1,6)
+        print("\n")
+        print("\n")
         print("The enemy player has rolled their die and covers it with their hands. You roll yours...")
         time.sleep(3)
 
@@ -20,25 +22,30 @@ if typeofgame == "1":
         time.sleep(2)
 
         #getting your guess on what they rolled, higher or lower
-        print("\n"); guess = input("Enemy player - Did you roll higher or lower than me? ")
+        print("\n")
+        print("Enemy player: Did you get the higher or lower number? (just say higher or lower)")
+        guess = input("You: ")
         time.sleep(2)
 
         #checking if your guess was correct then printing the result
         print("The enemy player reveals their roll.")
-        if guess == "Higher" or "higher" and yournum > aiNum:
-            print("Correct")
-            print(aiNum)
+        if (guess == "Higher" or guess == "higher") and yournum > int(aiNum):
+            print("Correct"); print("\n")
+            print("The enemy player's die had the number " + str(aiNum) + " on it.")
 
-        elif guess == "Lower" or "lower" and yournum < aiNum:
-            print("Correct")
-            print(aiNum)
+        elif (guess == "Lower" or guess == "lower") and yournum < int(aiNum):
+            print("Correct"); print("\n")
+            print("The enemy player's die had the number " + str(aiNum) + " on it.")
 
-        elif yournum == aiNum:
-            print("Whoops, it's the same.")
+        elif yournum == int(aiNum):
+            print("Whoops, it's the same."); print("\n")
+            print("Both die had the number " + str(aiNum) + " on it.")
+            
 
         else:
             print("Wrong")
-
+            print("The enemy player's die had the number  " + str(aiNum) + " on it.")
+            
 elif typeofgame == "2":
 
     while True:
@@ -63,33 +70,34 @@ elif typeofgame == "2":
             print("I think you got higher."); print("You reveal your roll.")
             time.sleep(2)
 
-            if aiNum < yournum:
-                print("correct")
-                print(yournum)
-            elif aiNum > yournum:
-                print("Wrong")
-                print(yournum)
-        elif aiGuess == 2:
+            if aiNum < int(yournum):
+                print("correct"); print("\n")
+                print("You had the number " + str(yournum) + " on it")
+            elif aiNum > int(yournum):
+                print("Wrong");print("\n")
+                print("You had the number " + str(yournum) + " on it")        
             print("I think you got lower."); print("You reveal your roll.")
-            if aiNum > yournum:
-                print("correct")
-                print(yournum)
-            elif aiNum < yournum:
-                print("Wrong")
-                print(yournum)
+        elif aiGuess == 2:
+            if aiNum > int(yournum):
+                print("correct");print("\n")
+                print("You had the number " + str(yournum) + " on it")
+            elif aiNum < int(yournum):
+                print("Wrong");print("\n")
+                print("You had the number " + str(yournum) + " on it")
         
 
         #if the roll was the same it would end in a draw
-        elif aiNum == yournum:
+        elif aiNum == int(yournum):
             if aiGuess == 1:
-                print("I think you got higher. You reveal the roll.")
-                print("Whoops, it's the same")
+                print("I think you got higher. You reveal the roll.");print("\n")
+                print("Whoops, it's the same. Both die had the number " + str(yournum) + " on them")
             elif aiGuess == 2:
-                print("I think you got lower. You reveal the roll.")
-                print("Whoops, it's the same")
+                print("I think you got lower. You reveal the roll.");print("\n")
+                print("Whoops, it's the same. Both die had the number " + str(yournum) + " on them")
             
 
 #if you chose anything other than 1 or 2 it wouldn't work
 else:
     print("Not valid option. Restart the program and pick either 1 or 2. ")
+
 
